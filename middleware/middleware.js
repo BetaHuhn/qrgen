82189430660233fab22aeb767c734146241b8ee3
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-05-07 01:16:50 +0200

    use let/const instead of var

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,16 +4,16 @@ module.exports = {
     log: () => {
         return (req, res, next) => {
             const ip = getClientIp(req)
-            let date_ob = new Date();
-            let date = ("0" + date_ob.getDate()).slice(-2);
-            let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-            let year = date_ob.getFullYear();
-            let hours = date_ob.getHours();
-            let minutes = date_ob.getMinutes();
-            let seconds = date_ob.getSeconds();
-            let milli = date_ob.getMilliseconds();
-            var time = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds + "." + milli;
-            console.log(time + " " + req.method + " " + req.originalUrl + ' request from: ' + ip);
+            const date_ob = new Date();
+            const date = ("0" + date_ob.getDate()).slice(-2);
+            const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+            const year = date_ob.getFullYear();
+            const hours = date_ob.getHours();
+            const minutes = date_ob.getMinutes();
+            const seconds = date_ob.getSeconds();
+            const milli = date_ob.getMilliseconds();
+            const time = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}.${milli}`;
+            console.log(`${time} ${req.method} ${req.originalUrl} request from ${ip}`);
             next()
         }
     }
