059d3120b5bcf2366ff2a6af614b14c64f696c9a
Maximilian Schiller <mail@creerow.de>
2020-05-18 14:53:14 +0200

    Removed extended url validation

--- utils/validUrl.js
+++ utils/validUrl.js
@@ -1,12 +1,12 @@
-const urlExists = require('url-exists-deep').default
+//const urlExists = require('url-exists-deep').default
 
 module.exports = async function (url){
     url = (!/^https?:\/\//i.test(url)) ? ("https://" + url) : url;
     var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
     var regexp = new RegExp(expression);
     if(regexp.test(url)){
-        var result = await urlExists(url);
-        return result
+        //var result = await urlExists(url);
+        return url //result
     }else{
         return false
     }
