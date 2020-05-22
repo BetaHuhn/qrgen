49ec9d5345eb8d3c586230fd28caf6767651204b
Alexander Janke <33967771+alexanderjanke@users.noreply.github.com>
2020-05-22 11:28:20 +0200

    Add type definitions for mongoose model "short"
    - Change "addedAt" to "createdOn" and type to number (timestamp)
    - Remove "currentDate" util and just use `+new Date()` to create the current time

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,7 +1,15 @@
-let mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+import mongoose, { Document, Schema } from "mongoose";
 
-let schema = new mongoose.Schema({
+export interface IShort extends Document {
+  _id: string;
+  code: string;
+  url: string;
+  count: number;
+  createdOn: number;
+  increase: Function;
+}
+
+const shortSchema: Schema = new mongoose.Schema({
   _id: Schema.Types.ObjectId,
   code: {
     type: String,
@@ -16,14 +24,14 @@ let schema = new mongoose.Schema({
   count: {
     type: Number,
   },
-  addedAt: {
-    type: Date,
+  createdOn: {
+    type: Number,
     required: true,
   },
 });
 
-schema.methods.increase = function () {
+shortSchema.methods.increase = function () {
   this.count += 1;
 };
 
-export default mongoose.model("Short", schema);
+export default mongoose.model<IShort>("Short", shortSchema);
