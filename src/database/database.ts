5fe584915c5c927ef98360f06f5fcb98e4abdd01
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-06-12 16:52:26 +0200

    Use mongodb connection string instead of individual values

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,23 +1,19 @@
 import mongoose from "mongoose";
 import log from "../utils/log";
 
-const server = process.env.DB_ADDRESS || "localhost:27017";
-const database = process.env.DB;
 const options = {
-  user: process.env.DB_USERNAME,
-  pass: process.env.DB_PASSWORD,
   keepAlive: true,
   keepAliveInitialDelay: 300000,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-const url = `mongodb://${server}/${database}?authSource=admin`;
+const url = <string> process.env.DB_CONNECTION_STRING;
 
 export async function connectDatabase() {
   await mongoose
     .connect(url, options)
     .then(() => {
-      log.log("Database connection successfull: " + database);
+      log.log("Database connection successfull");
       return mongoose;
     })
     .catch((err) => {
