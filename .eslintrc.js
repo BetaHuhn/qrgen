7b6eab64fbc04951e4256029c28d92a3157974b9
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2021-01-01 22:41:09 +0100

    ♻️ Use semantic-release

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    ...require('@betahuhn/config').eslint,
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        "@typescript-eslint"
+    ],
+    extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    rules: {
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/explicit-module-boundary-types": 0,
+        "typescript-eslint/no-var-requires": 0
+    }
+}
